c5418c98a21469c262ecfb09c3aa91c10656b71f
pavan530 <pavan.530530@gmail.com>
2022-01-31 16:14:27 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
-consumer_key = 'RmDHlUkiz7oCXNja70EOJpQNC'
-consumer_secret = 'HwRGErT2ONHDYqzovA0DyhGESHZW9FKlbQ4zVlDJ2BetxL055h'
-access_token = '2415807788-A13fbTX8mTRMaeo27nxxg84CZl7FH7uiqOCSdy2'
-access_token_secret = 'uT99vkDgavFHz7MXl0CUmEx1z5tjCYPfQRnnxFcoWGRQ8'
+consumer_key = 'XXXXXX'
+consumer_secret = 'XXXXXXXXX'
+access_token = '22334567ghjk'
+access_token_secret = 'fghdjkisgdhjf'
 es_host_name = "localhost:9200"
